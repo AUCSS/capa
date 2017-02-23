89a64d10e8fb1924fee9b248a6d3987d017174db
Keeyan Nejad <keeyan@knejad.co.uk>
2017-02-23 10:36:10 +0000

    Updated time.rb to be a little clearer

--- modules/time/time.rb
+++ modules/time/time.rb
@@ -1,1 +1,2 @@
-a=ARGV;o=a.empty? ? [0,0]: a[0].split(":").to_a;puts (Time.now+o[0].to_i*3600+(o[0][0]=="-"?-o[1].to_i*60: o[1].to_i*60)).strftime "%H:%M"
+o=ARGV.empty? ? [0,0]: ARGV[0].split(":").to_a
+puts (Time.now+o[0].to_i*3600+(o[0][0]=="-"?-o[1].to_i*60: o[1].to_i*60)).strftime "%H:%M"
